a079c2a3ca5ffb988f7449102effcdb9e051a2d4
atharvez <7atharvadesai@gmail.com>
2024-09-23 14:54:31 +0530

    modified:   pages/seva.js

--- pages/seva.js
+++ pages/seva.js
@@ -34,7 +34,6 @@ export default function Solo() {
                 className="w-full h-auto object-cover rounded-t-lg transition-opacity duration-300 ease-in-out opacity-100 group-hover:opacity-50"
               />
             )}
-            {/* Dark overlay on hover */}
             <div className="absolute inset-0 bg-black bg-opacity-30 opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out"></div>
             <p className="absolute inset-0 flex items-center justify-center text-center text-white font-medium opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out p-4">
               {photo.text}
