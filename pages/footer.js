35f6027804357110b026e190f91616b2dc113b5d
atharvez <7atharvadesai@gmail.com>
2024-09-23 14:44:30 +0530

    modified:   pages/footer.js 	modified:   pages/index.js

--- pages/footer.js
+++ pages/footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
       <footer className="bg-sky-950 text-white py-4">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <p className="text-center text-sm">
-            &copy; {new Date().getFullYear()} Department of Computer Engineering Regional Language. All rights reserved.
+            &copy; {new Date().getFullYear()} Department of Computer Engineering Regional Language |PCCCOE. All rights reserved.
           </p>
           <p className="text-center text-xs">By Atharva Atul Desai</p>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Layout from '../components/Layout';
 export default function Home() {
   return (
     <Layout>
-    <div>
+    <div src = '/images/pccoe'>
       <h2 className="text-2xl font-semibold justify-center">Universal Human Values</h2>
       <p className="mt-2 text-blue-700">This page is about the UHV Activites done by students of SY B.Tech Computer Engineering Regional Language.</p>
       </div>
